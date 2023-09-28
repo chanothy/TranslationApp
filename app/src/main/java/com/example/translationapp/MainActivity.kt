ce5ce0ba1f48308f2cfe085a030f50df03e84ce4
timchan <timchan@iu.edu>
2023-09-28 15:57:21 -0400

    view binding for translation fragment

--- app/src/main/java/com/example/translationapp/MainActivity.kt
+++ app/src/main/java/com/example/translationapp/MainActivity.kt
@@ -9,9 +9,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
+
         setContentView(view)
     }
 }
--- app/src/main/java/com/example/translationapp/TranslationFragment.kt
+++ app/src/main/java/com/example/translationapp/TranslationFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.translationapp.databinding.FragmentTranslationBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,20 +22,29 @@ class TranslationFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentTranslationBinding? = null
+    private val binding get() = _binding!!
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        arguments?.let {
+//            param1 = it.getString(ARG_PARAM1)
+//            param2 = it.getString(ARG_PARAM2)
+//        }
+//    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_translation, container, false)
+//        return inflater.inflate(R.layout.fragment_translation, container, false)
+        _binding = FragmentTranslationBinding.inflate(inflater,container,false)
+        val view = binding.root
+
+        return view
+
+
     }
 
     companion object {
